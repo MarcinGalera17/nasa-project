9cbcbe8fe559f67b6168243dba89a0b6da1ebd87
Marcin <jackdanielsgj@gmail.com>
2022-11-12 10:36:55 +0100

    done

--- src/Components/DogListWithInput.jsx
+++ src/Components/DogListWithInput.jsx
@@ -30,13 +30,19 @@ const DogListWithInput = () => {
 
   return (
     <div className={styles.wrapper}>
-      <div>
+      <div className={styles.inputAndSelectWrapper}>
         <select onChange={(e) => setbreedName(e.target.value)}>
           {breedList.map((breed) => {
             return <option key={breed}>{breed}</option>;
           })}
         </select>
-        <button onClick={fetchDogImage}>Seach</button>
+        <button
+          onClick={fetchDogImage}
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
       </div>
       {dogImage && (
         <div className={styles.dogImageContent}>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  content: [
+    // ...
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+};
